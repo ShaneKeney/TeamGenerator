b6da07ebd20da9faf73349d1144c233e09b865ad
Shane Keney <shane@spikeball.com>
2020-02-07 14:36:58 -0500

    - created Employee class - properties and methods

---
+++ models/Employee.js
@@ -0,0 +1,25 @@
+class Employee {
+    constructor(id, name, title) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+    }
+
+    getName() {
+
+    }
+
+    getId() {
+
+    }
+
+    getEmail() {
+
+    }
+
+    getRole() {
+
+    }
+}
+
+module.exports = Employee;
